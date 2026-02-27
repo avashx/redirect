c079b40cc8eb5f81542db73cf8685430cc337f1c
avashx <amanvash.official@gmail.com>
2026-02-27 20:40:26 +0530

    Add suspension redirect page with Node server

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const indexHtml = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  res.end(indexHtml);
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
